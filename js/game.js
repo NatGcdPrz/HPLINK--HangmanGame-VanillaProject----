6ec9a3ac70200fe2d4e0743853c96b2fad804535
Nathalie Perez <natgcdprz@icloud.com>
2023-01-15 19:53:02 +0100

    Modif

--- js/game.js
+++ js/game.js
@@ -11,130 +11,139 @@ const songWin = "/music/notification-win.mp3";
 const gameOnBtn = document.getElementById('game-on');
 
 const figureParts = document.querySelectorAll('.figure-part');
+const API_URL = 'https://random-word-api.herokuapp.com/word?number=200&lang=en';
 
-const words = ['application', 'programming', 'interface', 'hospitalink', 'internet', 'multimedia', 'javascript', 'logiciel', 'framework', 'toile', 'referencement'];
+async function getWords() {
+    const data = await fetch(API_URL);
 
-let selectedWord = words[Math.floor(Math.random() * words.length)];
+    const dataToJson = data.json();
+    console.log(dataToJson);
+    return dataToJson;
+}
 
-let playable = true;
+const play = async () => {
+    let words = await getWords();
+    let niceTryAdam = words[Math.floor(Math.random() * words.length)];
+    let playable = true;
 
-const correctLetters = [];
-const wrongLetters = [];
+    const correctLetters = [];
+    const wrongLetters = [];
 
-// Show hidden word
-function displayWord() {
-    wordEl.innerHTML = `
-    ${selectedWord
-            .split('')
-            .map(
-                letter => `
+    // Show hidden word
+    function displayWord() {
+        wordEl.innerHTML = `
+    ${niceTryAdam
+                .split('')
+                .map(
+                    letter => `
           <span class="letter">
             ${correctLetters.includes(letter) ? letter : ''}
           </span>
         `
-            )
-            .join('')}
+                )
+                .join('')}
   `;
 
-    const innerWord = wordEl.innerText.replace(/[ \n]/g, '');
+        const innerWord = wordEl.innerText.replace(/[ \n]/g, '');
 
-    if (innerWord === selectedWord) {
-        finalMessage.innerText = 'Congratulations! You won! 😃';
+        if (innerWord === niceTryAdam) {
+            finalMessage.innerText = 'Congratulations! You won! 😃';
 
-        popup.style.display = 'flex';
+            popup.style.display = 'flex';
 
-        playable = false;
-        new Audio(songWin).play();
+            playable = false;
+            new Audio(songWin).play();
+        }
     }
-}
 
-// Update the wrong letters
-function updateWrongLettersEl() {
-    // Display wrong letters
-    wrongLettersEl.innerHTML = `
+    // Update the wrong letters
+    function updateWrongLettersEl() {
+        // Display wrong letters
+        wrongLettersEl.innerHTML = `
     ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
     ${wrongLetters.map(letter => `<span>${letter}</span>`)}
   `;
 
-    // Display parts
-    figureParts.forEach((part, index) => {
-        const errors = wrongLetters.length;
+        // Display parts
+        figureParts.forEach((part, index) => {
+            const errors = wrongLetters.length;
 
-        if (index < errors) {
-            part.style.display = 'block';
-        } else {
-            part.style.display = 'none';
-        }
-    });
-
-    // Check if lost
-    if (wrongLetters.length === figureParts.length) {
-        finalMessage.innerText = 'Unfortunately you lost. 😕';
-        finalMessageRevealWord.innerText = `...the word was: ${selectedWord}`;
-        popup.style.display = 'flex';
-
-        playable = false;
-        new Audio(song).play();
-    }
-}
-
-
-// Show notification
-function showNotification() {
-    notification.classList.add('show');
-
-    setTimeout(() => {
-        notification.classList.remove('show');
-    }, 2000);
-}
-
-// Keydown letter press
-window.addEventListener('keydown', e => {
-    if (playable) {
-        if (e.keyCode >= 65 && e.keyCode <= 90) {
-            const letter = e.key.toLowerCase();
-
-            if (selectedWord.includes(letter)) {
-                if (!correctLetters.includes(letter)) {
-                    correctLetters.push(letter);
-
-                    displayWord();
-                } else {
-                    showNotification();
-                }
+            if (index < errors) {
+                part.style.display = 'block';
             } else {
-                if (!wrongLetters.includes(letter)) {
-                    wrongLetters.push(letter);
+                part.style.display = 'none';
+            }
+        });
 
-                    updateWrongLettersEl();
+        // Check if lost
+        if (wrongLetters.length === figureParts.length) {
+            finalMessage.innerText = 'Unfortunately you lost. 😕';
+            finalMessageRevealWord.innerText = `...the word was: ${niceTryAdam}`;
+            popup.style.display = 'flex';
+
+            playable = false;
+            new Audio(song).play();
+        }
+    }
+
+
+    // Show notification
+    function showNotification() {
+        notification.classList.add('show');
+
+        setTimeout(() => {
+            notification.classList.remove('show');
+        }, 2000);
+    }
+
+
+    // Keydown letter press
+    window.addEventListener('keydown', e => {
+        if (playable) {
+            if (e.keyCode >= 65 && e.keyCode <= 90) {
+                const letter = e.key.toLowerCase();
+
+                if (niceTryAdam.includes(letter)) {
+                    if (!correctLetters.includes(letter)) {
+                        correctLetters.push(letter);
+
+                        displayWord();
+                    } else {
+                        showNotification();
+                    }
                 } else {
-                    showNotification();
+                    if (!wrongLetters.includes(letter)) {
+                        wrongLetters.push(letter);
+
+                        updateWrongLettersEl();
+                    } else {
+                        showNotification();
+                    }
                 }
             }
         }
-    }
-});
+    });
 
-// Restart game and play again
-playAgainBtn.addEventListener('click', () => {
-    playable = true;
+    // Restart game and play again
+    playAgainBtn.addEventListener('click', () => {
+        playable = true;
 
 
-    //  Empty arrays
-    correctLetters.splice(0);
-    wrongLetters.splice(0);
+        //  Empty arrays
+        correctLetters.splice(0);
+        wrongLetters.splice(0);
 
-    selectedWord = words[Math.floor(Math.random() * words.length)];
+        niceTryAdam = words[Math.floor(Math.random() * words.length)];
 
+        displayWord();
+
+        updateWrongLettersEl();
+
+
+        popup.style.display = 'none';
+
+    });
     displayWord();
+}
 
-    updateWrongLettersEl();
-
-
-    popup.style.display = 'none';
-
-});
-
-displayWord();
-
-
+play();
